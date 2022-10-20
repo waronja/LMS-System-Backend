9063d3464c39fdf8fabb65c8a48145b89a4b4d18
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-10-20 13:30:22 +0300

    created a resource

---
+++ db/migrate/20221020102951_create_resources.rb
@@ -0,0 +1,11 @@
+class CreateResources < ActiveRecord::Migration[7.0]
+  def change
+    create_table :resources do |t|
+      t.string :name
+      t.integer :lesson_id
+      t.integer :quiz_id
+
+      t.timestamps
+    end
+  end
+end
