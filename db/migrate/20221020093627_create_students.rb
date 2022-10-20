3b31477d9ff013b88dac4cb8670a47070da64a41
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-10-20 12:40:28 +0300

    created student

--- db/migrate/20221020093627_create_students.rb
+++ db/migrate/20221020093627_create_students.rb
@@ -1,12 +1,12 @@
-class CreateSchoolOwners < ActiveRecord::Migration[7.0]
+class CreateStudents < ActiveRecord::Migration[7.0]
   def change
-    create_table :school_owners do |t|
+    create_table :students do |t|
       t.string :first_name
       t.string :last_name
       t.string :email
       t.string :password_digest
       t.integer :school_id
-      t.boolean :isadmin,default:true
+      t.boolean :isadmin,default:false
 
       t.timestamps
     end
