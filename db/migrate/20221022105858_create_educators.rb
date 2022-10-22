e915e9c3bffd827ca8e6e806dfd4be8300315877
Judy Mwai <juddiemukami@gmail.com>
2022-10-22 14:16:28 +0300

    adds migrations

--- db/migrate/20221022105858_create_educators.rb
+++ db/migrate/20221022105858_create_educators.rb
@@ -6,7 +6,9 @@ class CreateEducators < ActiveRecord::Migration[7.0]
       t.string :email
       t.string :password_digest
       t.integer :school_id
-      t.boolean :isadmin
+      t.boolean :isadmin,default:false
+      
+      
 
       t.timestamps
     end
