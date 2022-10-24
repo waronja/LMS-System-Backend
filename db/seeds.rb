ed8ecae8f09384d8f05a344a1a105c54a6655c8c
Judy Mwai <juddiemukami@gmail.com>
2022-10-24 13:18:23 +0300

    delete seeding

--- db/seeds.rb
+++
@@ -1,4 +0,0 @@
-school1 = School.create(name:"School of Software development", description:"Be a professional developer")
-school2 = School.create(name:"School of Data science", description:"Be a data analyst")
-
-puts "done seeding"
