9f96ed2cebdba74f25d87abe261040169c311525
gideonlangat98 <gideonlangat98@gmail.com>
2022-10-31 14:40:28 +0300

    change production commands

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -42,12 +42,9 @@ class LessonsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def lesson_params
-<<<<<<< HEAD
-      params.permit(:name, :chat_id, :assessment_id, :phase_id)
-=======
       params.permit(:name, :phase_id, :educator_id, :student_id)
->>>>>>> e49da05c105fbd316556140730bc7caa78f37439
     end
+    
     def authorize
       return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :educator_id
     end
