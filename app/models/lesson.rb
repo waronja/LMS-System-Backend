1be3adb18e62f73c991ddc771a434c68a8ea021f
gideonlangat98 <gideonlangat98@gmail.com>
2022-11-02 09:33:34 +0300

    lesson seed

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,10 +1,10 @@
 class Lesson < ApplicationRecord
 validates :name, presence: true
 
-    # belongs_to :student
+    belongs_to :student
     # has_one :assessment
     belongs_to :phase
     belongs_to :educator
-    has_many :resources
+    # has_many :resources
 
 end
